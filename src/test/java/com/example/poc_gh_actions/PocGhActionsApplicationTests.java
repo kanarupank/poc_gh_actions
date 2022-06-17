14cb845b0fcc7fd80d3413e8b7386068069e1647
kanarupan <kanarupank.20@uom.lk>
2022-06-17 23:38:25 +0530

    init

---
+++ src/test/java/com/example/poc_gh_actions/PocGhActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc_gh_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGhActionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
